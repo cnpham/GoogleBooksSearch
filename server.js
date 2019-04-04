89eb7e58af4c5237011a8ce2926c1fe62840508a
cnpham <44155537+cnpham@users.noreply.github.com>
2019-04-04 03:21:39 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
+var mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+var db = require("googlebooks");
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
